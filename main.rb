31a645052d994ae2f00a244ce8fc23e520bd0baf
Collins <mussocollins@gmail.com>
2022-10-18 13:16:18 +0300

    Make changes to main.rb

--- main.rb
+++ main.rb
@@ -2,14 +2,14 @@ def main
     puts 'Welcome to catalog of things: To use the app choose from the following'
     loop do
       puts 'Please choose an option by entering a number:'
-      puts '1 - List of Books
-       2 - List all labels (e.g. Gift, 'New')
+      puts '1 - List
+       2 - List all labels
        3 - Add a book
        4 - List all music albums
-       5 - List all genres (e.g 'Comedy', 'Thriller')
+       5 - List all genres
        6 - Add a music album
        7 - List of games
-       8 - List all authors (e.g. 'Stephen King')
+       8 - List all authors
        9 - Add a game
        10 - Exit'
       choice = gets.chomp.to_i
@@ -17,7 +17,6 @@ def main
         puts 'Thank you for using our App'
         break
       end
-      start_app(choice)
     end
   end
   
